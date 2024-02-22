f9b5f565a5dc89210277f6d36b4863a1d667064d
rishabhbizzle <rishabhbizzle@gmail.com>
2024-02-22 19:09:32 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <p>{import.meta.env.VITE_TEST}</p>
+      <p>Env Variable: {import.meta.env.VITE_TEST}</p>
     </>
   )
 }
